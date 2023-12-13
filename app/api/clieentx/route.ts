12413c2c52ff798080a5e66fc3a7fc8905af5c3f
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2023-12-13 12:29:21 +0200

    excluir api routes

--- app/api/clieentx/route.ts
+++ app/api/clieentx/route.ts
@@ -8,9 +8,3 @@ export async function GET(request: Request) {
   return NextResponse.json(respo)
 }
 
-// import { NextResponse } from 'next/server'
- 
-// export async function POST(request: Request) {
-//   const res = await request.json()
-//   return NextResponse.json({ res })
-// }
