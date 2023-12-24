9ac6aa83635607af6dc2bc5a073f6561fb8a555a
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2023-12-24 03:24:54 +0200

    clientes1

--- app/(administracao)/clientes/page.tsx
+++ app/(administracao)/clientes/page.tsx
@@ -19,23 +19,21 @@
 //   zona: string;
 // }
 
-// type ZonasProps = {
-//   zonas: Zona[];
-// }
 
-// async function getDataClientes(){
+
+async function getDataClientes(){
   
-//   const res = await fetch(`https://agua-front.vercel.app/api/todos`,{cache:"no-cache"})
+  const res = await fetch(`https://agua-front.vercel.app/api/todos`,{cache:"no-cache"})
 
-//   if(!res.ok){
-//     throw new Error('Failed to fetch data')
-//   }
-//   return res.json()
-// }
+  if(!res.ok){
+    throw new Error('Failed to fetch data')
+  }
+  return res.json()
+}
 
 export default async function Zona() {
 
-  // const responseC= await getDataClientes();
+  const responseC= await getDataClientes();
   
   // const clients = await responseC.allClientes
   // const numero  = await responseC.allClientes.filter((cliente:any) => cliente.estado === "Activo").length
