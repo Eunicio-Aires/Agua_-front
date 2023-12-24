1c811dc7f3a6b0b7c8b1193e2c60644d518044a8
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2023-12-24 22:20:57 +0200

    edicao do chart

--- app/(administracao)/pagaments/page.tsx
+++ app/(administracao)/pagaments/page.tsx
@@ -1,7 +1,7 @@
 
 import  Header  from '@/components/Header'
 import { Ultimom } from '@/components/payments/Ultimom'
-import Charts from '@/components/Charts'
+import Chart from '@/components/Charts'
 import  Pagamentos  from '@/components/payments/Pagamentos'
 import Meses      from '@/components/payments/Meses'
 import Link from "next/link"
@@ -33,7 +33,7 @@ export default async  function Pagaments(){
             
             <div className='w-80'>
             
-            <Charts/>
+            <Chart/>
 
              {/* <div className="overflow-x-auto p-4">
     <table className="table table-xs table-pin-rows table-pin-cols">
