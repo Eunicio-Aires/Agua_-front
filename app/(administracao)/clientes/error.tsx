1b643e7ffa98dda09c8d1ea2c58d0eef76e5c34b
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2023-12-27 15:10:23 +0200

    edited

--- app/(administracao)/clientes/error.tsx
+++ app/(administracao)/clientes/error.tsx
@@ -4,8 +4,8 @@ export default function error(error:any){
     return(
         <>
 
-        <p>Erro na ao efetuar a busca</p>
-            {/* <p>{error.error.message}</p> */}
+        {/* <p>Erro na ao efetuar a busca</p> */}
+            <p>{error.error.message}</p>
         </>
     )
 }
