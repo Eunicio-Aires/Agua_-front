0e56149ea67110ce9103fa9666db70870bc7732f
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2023-12-27 16:46:15 +0200

    mudando de tab

--- app/(administracao)/clientes/[id]/page.tsx
+++ app/(administracao)/clientes/[id]/page.tsx
@@ -80,16 +80,16 @@ import OtherUsers from "@/components/outros";
       <div> 
 
 
-              <div role="tablist" className="tabs tabs-bordered">
-                <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="Tab 1" />
-                <div role="tabpanel" className="tab-content p-10">Tab content 1</div>
+<div role="tablist" className="tabs tabs-lifted">
+  <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 1" />
+  <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 1</div>
 
-                <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="Tab 2" checked />
-                <div role="tabpanel" className="tab-content p-10">Tab content 2</div>
+  <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 2" checked />
+  <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 2</div>
 
-                <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="Tab 3" />
-                <div role="tabpanel" className="tab-content p-10">Tab content 3</div>
-              </div>
+  <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 3" />
+  <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 3</div>
+</div>
            <div>{dados} </div>
            {/* <div>My Post: {params.id}</div> */}
        {/* <TopCards/>  */}
