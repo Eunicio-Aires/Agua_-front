3de8cde3d3a5cba2e55fa4f020255b6378044ab1
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2023-12-27 17:38:42 +0200

    adicionando tabela

--- app/(administracao)/clientes/loading.tsx
+++ app/(administracao)/clientes/loading.tsx
@@ -2,9 +2,9 @@ export default function loading(){
 
     return(
 
+       <div className="flex items-center justify-center h-screen">
+         <span className="loading loading-dots loading-lg"> </span>
+       </div>
        
-        <span className="loading loading-dots loading-lg"> 
-            
-        </span>
     )
 }
