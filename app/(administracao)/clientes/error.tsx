90e1ca5029d2a17e56952910c76923d8987f37a2
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2023-12-27 16:19:54 +0200

    abilitando error page

--- app/(administracao)/clientes/error.tsx
+++ app/(administracao)/clientes/error.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-// export default function error(error:any){
-//     return(
-//         <>
+export default function error(error:any){
+    return(
+        <>
 
-//         {/* <p>Erro na ao efetuar a busca</p> */}
-//             <p>{error.error.message}</p>
-//         </>
-//     )
-// }
+       
+            <p>{error.error.message}</p>
+        </>
+    )
+}
