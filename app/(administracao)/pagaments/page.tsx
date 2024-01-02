b18c2c298fc626daf31559588f4604d8c769c7b4
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-01-02 13:51:11 +0200

    tipagem do Mes

--- app/(administracao)/pagaments/page.tsx
+++ app/(administracao)/pagaments/page.tsx
@@ -21,6 +21,17 @@ interface Fatura {
   valor?: number;
 }
 
+
+
+
+interface Mes {
+  _id: string;
+  mes: string;
+  faturas: Fatura[];
+  createdAt: string;
+  __v: number;
+}
+
 // const url = 'https://agua-front.vercel.app/api/faturar';
 
 
@@ -114,7 +125,7 @@ export default async  function Pagaments(){
     </div>
   </div>
   <div className="flex flex-wrap -m-4">
-  {meses.map((mes:Fatura)=>(
+  {meses.map((mes:Mes)=>(
      
     <div key={mes._id} className="xl:w-1/3 md:w-1/2 p-4 border-8 rounded-xl"> 
     
@@ -123,16 +134,16 @@ export default async  function Pagaments(){
         <h3 className="tracking-widest tx-gradient-to-r from-red-700 to-indigo-600 text-xs font-medium title-font">{(mes.faturas.filter((fatura: { estado: any; }) => fatura.estado === "Pago").length /mes.faturas.length )*100}%</h3>
         <h1 className="text-lg text-gray-900 font-medium title-font mb-4">{mes.mes}</h1>
 
-        <p className="leading-relaxed text-base">Nao pagas{mes.faturas.filter((fatura: { estado: any; }) => fatura.estado === "Nao pago").length}</p>
+        <p className="leading-relaxed text-base">Nao pagas{mes.faturas.filter((fatura: { estado: String ; }) => fatura.estado === "Nao pago").length}</p>
 
         <p>
        </p>
     
 
 
-        <p className="leading-relaxed text-base">Pagas{mes.faturas.filter((fatura: { estado: any; }) => fatura.estado === "Pago").length}</p>
+        <p className="leading-relaxed text-base">Pagas{mes.faturas.filter((fatura: { estado: String; }) => fatura.estado === "Pago").length}</p>
         
-        <p className="leading-relaxed text-base">Pagas{mes.faturas.filter((fatura: { estado: any; }) => fatura.estado === "Pago")}</p> 
+        {/* <p className="leading-relaxed text-base">Pagas{mes.faturas.filter((fatura: { estado: String; }) => fatura.estado === "Pago")}</p>  */}
         <Link href={`/faturar/${mes._id}`} className="middle none center mr-4 rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
         data-ripple-light="true">Faturar</Link>
       </div>
