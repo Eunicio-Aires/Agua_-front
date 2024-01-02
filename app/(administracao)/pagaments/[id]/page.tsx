ca09a77231afcaf7e52461a76ea4288a2ac8b7c3
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-01-02 20:51:10 +0200

    confirmacao do obj

--- app/(administracao)/pagaments/[id]/page.tsx
+++ app/(administracao)/pagaments/[id]/page.tsx
@@ -1,22 +1,23 @@
 
 
-async function MesData(){
-    const res = await fetch(`https://agua-p.vercel.app/adm/meses`,{cache:"no-cache"})
+// async function MesData(){
+//     const res = await fetch(`https://agua-p.vercel.app/adm/meses`,{cache:"no-cache"})
    
   
-  if(!res.ok){
-    throw new Error('Failed to fetch data')
-  }
-  return res.json()
+//   if(!res.ok){
+//     throw new Error('Failed to fetch data')
+//   }
+//   return res.json()
 
-}
+// }
 export default async function UnicoMes({ params }: { params: { id: string } }){
     const id = params.id
-    const res = await fetch(`https://agua-p.vercel.app/adm/mesunic/${id}`,{cache:"no-cache"})
+    const res = await fetch(`https://agua-p.vercel.app/adm/mesunic/${id}`,{cache:"no-cache"}).then((res) => res.json())
+    
 
     return(
         <>
-            <p>My Post: {id}</p>
+            <p>My Post: {res.mes.id}</p>
         </>
     )
 }
