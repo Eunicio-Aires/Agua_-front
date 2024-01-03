992e41bae6a744b4f77fec124bb3e4dad69fadd4
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-01-03 15:28:56 +0200

    atualizard

--- app/(administracao)/pagaments/[id]/page.tsx
+++ app/(administracao)/pagaments/[id]/page.tsx
@@ -61,6 +61,11 @@ export default async function UnicoMes({ params }: { params: { id: string } }){
     // Calcular a soma dos valores das faturas nao pagas
     const somaValoresFaturasNaoPagas: number = await faturasNaoPagas.reduce((soma, fatura) => soma + (fatura.valor || 0), 0);
 
+   //  formas de pagamento
+   const faturasNumerario: Fatura[] = todasFaturas.filter((fatura: Fatura) => fatura.formaDePagamento === "Numerario");
+   const numeroFaturasNumerario: number = faturasNumerario.length;
+   const somaValoresFaturasNumerario: number = faturasNumerario.reduce((soma, fatura) => soma + (fatura.valor || 0), 0);
+
     
 
     return(
@@ -253,50 +258,7 @@ export default async function UnicoMes({ params }: { params: { id: string } }){
                      </div>
                   </div>
                </div>
-               <div className="mt-4 w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-                  <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
-                     <div className="flex items-center">
-                        <div className="flex-shrink-0">
-                           <span className="text-2xl sm:text-3xl leading-none font-bold text-gray-900">2,340</span>
-                           <h3 className="text-base font-normal text-gray-500">New products this week</h3>
-                        </div>
-                        <div className="ml-5 w-0 flex items-center justify-end flex-1 text-green-500 text-base font-bold">
-                           14.6%
-                           <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                              <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
-                           </svg>
-                        </div>
-                     </div>
-                  </div>
-                  <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
-                     <div className="flex items-center">
-                        <div className="flex-shrink-0">
-                           <span className="text-2xl sm:text-3xl leading-none font-bold text-gray-900">5,355</span>
-                           <h3 className="text-base font-normal text-gray-500">Visitors this week</h3>
-                        </div>
-                        <div className="ml-5 w-0 flex items-center justify-end flex-1 text-green-500 text-base font-bold">
-                           32.9%
-                           <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                              <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
-                           </svg>
-                        </div>
-                     </div>
-                  </div>
-                  <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
-                     <div className="flex items-center">
-                        <div className="flex-shrink-0">
-                           <span className="text-2xl sm:text-3xl leading-none font-bold text-gray-900">385</span>
-                           <h3 className="text-base font-normal text-gray-500">User signups this week</h3>
-                        </div>
-                        <div className="ml-5 w-0 flex items-center justify-end flex-1 text-red-500 text-base font-bold">
-                           -2.7%
-                           <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                              <path fill-rule="evenodd" d="M14.707 12.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l2.293-2.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
-                           </svg>
-                        </div>
-                     </div>
-                  </div>
-               </div>
+             
                <div className="grid grid-cols-1 2xl:grid-cols-2 xl:gap-4 my-4">
                   <div className="bg-white shadow rounded-lg mb-4 p-4 sm:p-6 h-full">
                      <div className="flex items-center justify-between mb-4">
@@ -406,14 +368,15 @@ export default async function UnicoMes({ params }: { params: { id: string } }){
                         <table className="items-center w-full bg-transparent border-collapse">
                            <thead>
                               <tr>
-                                 <th className="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">Top Channels</th>
-                                 <th className="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">Users</th>
-                                 <th className="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap min-w-140-px"></th>
+                                 <th className="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">Canais de Pagamento</th>
+                                 <th className="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">N de faturas</th>
+                                 <th className="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap min-w-140-px">Percentagem</th>
                               </tr>
                            </thead>
                            <tbody className="divide-y divide-gray-100">
                               <tr className="text-gray-500">
-                                 <th className="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">Organic Search</th>
+                                 <th className="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">M-pesa</th>
+                                 <th className="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">Faturas</th>
                                  <td className="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">5,649</td>
                                  <td className="border-t-0 px-4 align-middle text-xs whitespace-nowrap p-4">
                                     <div className="flex items-center">
@@ -427,8 +390,8 @@ export default async function UnicoMes({ params }: { params: { id: string } }){
                                  </td>
                               </tr>
                               <tr className="text-gray-500">
-                                 <th className="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">Referral</th>
-                                 <td className="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">4,025</td>
+                                 <th className="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">Numerario</th>
+                                 <td className="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">{somaValoresFaturasNumerario}</td>
                                  <td className="border-t-0 px-4 align-middle text-xs whitespace-nowrap p-4">
                                     <div className="flex items-center">
                                        <span className="mr-2 text-xs font-medium">24%</span>
@@ -441,7 +404,7 @@ export default async function UnicoMes({ params }: { params: { id: string } }){
                                  </td>
                               </tr>
                               <tr className="text-gray-500">
-                                 <th className="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">Direct</th>
+                                 <th className="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">Emola</th>
                                  <td className="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">3,105</td>
                                  <td className="border-t-0 px-4 align-middle text-xs whitespace-nowrap p-4">
                                     <div className="flex items-center">
@@ -454,48 +417,7 @@ export default async function UnicoMes({ params }: { params: { id: string } }){
                                     </div>
                                  </td>
                               </tr>
-                              <tr className="text-gray-500">
-                                 <th className="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">Social</th>
-                                 <td className="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">1251</td>
-                                 <td className="border-t-0 px-4 align-middle text-xs whitespace-nowrap p-4">
-                                    <div className="flex items-center">
-                                       <span className="mr-2 text-xs font-medium">12%</span>
-                                       <div className="relative w-full">
-                                          <div className="w-full bg-gray-200 rounded-sm h-2">
-                                             {/* <div className="bg-pink-600 h-2 rounded-sm" style="width: 12%"></div> */}
-                                          </div>
-                                       </div>
-                                    </div>
-                                 </td>
-                              </tr>
-                              <tr className="text-gray-500">
-                                 <th className="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">Other</th>
-                                 <td className="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">734</td>
-                                 <td className="border-t-0 px-4 align-middle text-xs whitespace-nowrap p-4">
-                                    <div className="flex items-center">
-                                       <span className="mr-2 text-xs font-medium">9%</span>
-                                       <div className="relative w-full">
-                                          <div className="w-full bg-gray-200 rounded-sm h-2">
-                                             <div className="bg-indigo-600 h-2 rounded-sm w-7" ></div>
-                                          </div>
-                                       </div>
-                                    </div>
-                                 </td>
-                              </tr>
-                              <tr className="text-gray-500">
-                                 <th className="border-t-0 align-middle text-sm font-normal whitespace-nowrap p-4 pb-0 text-left">Email</th>
-                                 <td className="border-t-0 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4 pb-0">456</td>
-                                 <td className="border-t-0 align-middle text-xs whitespace-nowrap p-4 pb-0">
-                                    <div className="flex items-center">
-                                       <span className="mr-2 text-xs font-medium">7%</span>
-                                       <div className="relative w-full">
-                                          <div className="w-full bg-gray-200 rounded-sm h-2">
-                                             {/* <div className="bg-purple-500 h-2 rounded-sm" style="width: 7%"></div> */}
-                                          </div>
-                                       </div>
-                                    </div>
-                                 </td>
-                              </tr>
+                              
                            </tbody>
                         </table>
                      </div>
