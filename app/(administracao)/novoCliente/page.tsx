74b2927e5dee23cf912164cb57cf63e33014ba2b
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-01-05 10:06:41 +0200

    alteracao do map

--- app/(administracao)/novoCliente/page.tsx
+++ app/(administracao)/novoCliente/page.tsx
@@ -3,7 +3,7 @@ export default async function NCliente(){
     const zonas = await fetch('https://agua-front.vercel.app/api/areas',{cache:'no-store'}).then((res) => res.json())
     return(
         <div>
-            {zonas.map((zona:any)=>(
+            {zonas.allAreas.map((zona:any)=>(
             <div  key={zona._id } >
                 contalksd
             </div>
