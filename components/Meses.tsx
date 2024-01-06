6173992e9c23f8a17e43fcfc7105bb858878b9d5
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-01-06 23:33:21 +0200

    data retirado

--- components/Meses.tsx
+++ components/Meses.tsx
@@ -27,7 +27,7 @@ return(
       </div>
     </div>
     <div className="flex flex-wrap -m-4">
-    {response.data.mesesl.map((mes:any)=>(
+    {response.mesesl.map((mes:any)=>(
       <div key= {mes._id} className="xl:w-1/3 md:w-1/2 p-4 border-8">
         <div className="bg-white p-6 rounded-lg">
           {/* <img className="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6" src="https://kuyou.id/content/images/ctc_2020021605150668915.jpg" alt="Image Size 720x400"> */}
