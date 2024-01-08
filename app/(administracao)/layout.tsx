10c9de481a0e045d1d8eec96d1867b8db20a6412
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-01-08 18:16:27 +0200

    faturas

--- app/(administracao)/layout.tsx
+++ app/(administracao)/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
     </div>
   </div>
   <div className="navbar-center">
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <a className="btn btn-ghost normal-case text-xl">Gestao de Faturas</a>
   </div>
   <div className="navbar-end">
     <button className="btn btn-ghost btn-circle">
