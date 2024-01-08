bd7ff5dabf4d48e46d87b4a256c927d2e01c98b5
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-01-08 19:06:00 +0200

    conf

--- app/(administracao)/novoCliente/page.tsx
+++ app/(administracao)/novoCliente/page.tsx
@@ -2,16 +2,7 @@ import Link from "next/link"
 export default async function NCliente(){
     const zonas = await fetch('https://agua-p.vercel.app/adm/allAreas',{cache:'no-store'}).then((res) => res.json())
     return(
-        <div>
-            Seleciona a Area
-            {zonas.allAreas.map((zona:any)=>(
-            <div  key={zona._id } >
-                <p>{zona._id}</p>
-                <p>{zona.zona}</p>
-                <Link href={`/novoCliente/${zona._id}`}>selecionar</Link>
-            </div>
-
-          ))}
+      
           
 <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
     <div className="text-center pb-12">
@@ -29,6 +20,7 @@ export default async function NCliente(){
             <div className="text-center">
                 <p className="text-xl text-gray-700 font-bold mb-2">{zona.zona}</p>
                 <p className="text-base text-gray-400 font-normal">{zona.cliente.length}</p>
+                <Link href={`/novoCliente/${zona._id}`}>selecionar</Link>
             </div>
         </div>
        
@@ -38,6 +30,6 @@ export default async function NCliente(){
 
 
 
-        </div>
+       
     )
 }
