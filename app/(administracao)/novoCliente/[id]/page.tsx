0526ebbcb2cfe149dbe787434126eff4b7f31b9c
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-01-08 20:08:59 +0200

    id adicionad

--- app/(administracao)/novoCliente/[id]/page.tsx
+++ app/(administracao)/novoCliente/[id]/page.tsx
@@ -3,9 +3,9 @@ export default async function UnicaZona({ params }: { params: { id: string } }){
      const posts = await fetch(`https://agua-p.vercel.app/adm/area/${params.id}`,{ cache: 'no-store'}).then((res) => res.json())
     return(
         <>
-        <p>{posts.area.zona}</p>
+        <p>{posts.area.zona}</p>r
 
-            <Registar id={posts.area.zona._id}/>
+            <Registar id={posts.area._id}/>
         </>
     )
 }
