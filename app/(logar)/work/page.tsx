8113abd361f96ccf88db65c44eb1e51485da2af7
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-01-13 03:09:56 +0200

    form editado

--- app/(logar)/work/page.tsx
+++ app/(logar)/work/page.tsx
@@ -1,4 +1,4 @@
-
+import Link from "next/link";
 export default function Work(){
 
     return (
@@ -8,13 +8,13 @@ export default function Work(){
     <div className="container mx-auto">
         <div className="max-w-md mx-auto my-10">
             <div className="text-center">
-                <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">Sign in</h1>
-                <p className="text-gray-500 dark:text-gray-400">Sign in to access your account</p>
+                <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">Entrar</h1>
+                <p className="text-gray-500 dark:text-gray-400">Navegacao segura </p>
             </div>
             <div className="m-7">
-                <form action="">
+                <form action="https://agua-front.vercel.app/pagaments">
                     <div className="mb-6">
-                        <label  className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Email Address</label>
+                        <label  className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Email </label>
                         <input type="email" name="email" id="email" placeholder="you@company.com" className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
                     </div>
                     <div className="mb-6">
@@ -25,7 +25,7 @@ export default function Work(){
                         <input type="password" name="password" id="password" placeholder="Your Password" className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
                     </div>
                     <div className="mb-6">
-                        <button type="button" className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none">Sign in</button>
+                        <button type="button" className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none">Log</button>
                     </div>
                     <p className="text-sm text-center text-gray-400">Don&#x27;t have an account yet? <a href="#!" className="text-indigo-400 focus:outline-none focus:underline focus:text-indigo-500 dark:focus:border-indigo-800">Sign up</a>.</p>
                 </form>
@@ -37,7 +37,7 @@ export default function Work(){
 
 
 <div className="fixed bottom-5 w-full text-center text-gray-400">
-	Crafted with ♡ by <a className="text-blue-500" target="_blank" href="https://web3templates.com/components/">Web3Templates</a>
+	Crafted with ♡ by <Link  href=""></Link> <p className="text-blue-500">E.Aires</p>
 </div>
         
         </>
