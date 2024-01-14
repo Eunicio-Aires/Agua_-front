7a7662aa329fb0f211faf2f4b132b265cc4c319b
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-01-14 14:15:38 +0200

    configued

--- components/pagamento/ButPagar.tsx
+++ components/pagamento/ButPagar.tsx
@@ -14,11 +14,12 @@ export default function ButPagar(){
         <>
         <div className="transition ease-in-out delay-150">
         <button onClick={buttonHandler}>
-          {iseBlueBoxShown ? <button className="btn btn-circle btn-outline">
+          {iseBlueBoxShown ? <button className="btn btn-circle btn-outline ml-2">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-            </button> : <p  className="btn btn-outline btn-accent"> Pagar <GiReceiveMoney /></p>}
+            </button > : <p  className="btn btn-outline btn-accent ml-2"> Pagar <GiReceiveMoney /></p>}
         </button>
         {iseBlueBoxShown && <div className="flex items-center justify-center p-6 " >
+        <Emola/>
         <Emola/>
         {/* onSubmit={handleSubmit} */}
         {/* <form  onSubmit={handleSubmit}>
