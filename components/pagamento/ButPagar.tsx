697c74c8b6d0a01f1ec708046351c4dd553f6e6b
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-01-14 14:33:32 +0200

    estelizacao retirada

--- components/pagamento/ButPagar.tsx
+++ components/pagamento/ButPagar.tsx
@@ -18,7 +18,7 @@ export default function ButPagar(){
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
             </button > : <p  className="btn btn-outline btn-accent ml-2"> Pagar <GiReceiveMoney /></p>}
         </button>
-        {iseBlueBoxShown && <div className="flex items-center justify-center p-6 " >
+        {iseBlueBoxShown && <div className=" p-6 " >
         <Emola/>
         <Emola/>
         {/* onSubmit={handleSubmit} */}
