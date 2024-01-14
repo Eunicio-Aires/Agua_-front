012d48d38d228893ac88a3d19bff3ba5b1839d66
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-01-14 13:54:04 +0200

    organized

--- app/(loggado)/pagamentos/[pcliente]/page.tsx
+++ app/(loggado)/pagamentos/[pcliente]/page.tsx
@@ -5,7 +5,6 @@ import { Footer } from '@/components/Foter'
 import { Modal } from '@/components/Modal'
 import { GiReceiveMoney } from "react-icons/gi";
 import Link from "next/link";
-import Emola from '@/components/pagamento/Emola';
 import ButPagar from '@/components/pagamento/ButPagar';
 
 import {BsPersonFill, BsThreeDotsVertical} from 'react-icons/bs'
@@ -29,31 +28,31 @@ export default async function Page({ params }: { params: {pcliente: Cliente}}){
     const fatura = await cliente.client.faturas
     const fatur = cliente.client.faturas.filter((fatura:Faturas) => fatura.estado === "Nao pago")
     const faturUl = cliente.client.faturas
-  .filter((fatura: Faturas) => fatura.estado === "Pago")
-  .slice(-5);
+   .filter((fatura: Faturas) => fatura.estado === "Pago")
+    .slice(-5);
    
     
     return(
 
-<section className="py-1 bg-blueGray-50">
+  <section className="py-1 bg-blueGray-50">
 
       
-<div className='h-28 bg-gradient-to-r from-indigo-400 to-cyan-400 static'> 
+      <div className='h-28 bg-gradient-to-r from-indigo-400 to-cyan-400 static'> 
 
             <h2 className='pt-2 mt-3  text-center  text-2xl font-bold'>{cliente.client.nome}</h2>
             <div className='flex justify-between px-4 pt-4 '>
             <p className="text-sm">P. Pagar: {fatur.length}</p>
             <h2>estado:{cliente.client.estado}</h2>
         </div>
-        </div>
+      </div>
 
 
 
-<div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
-  <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
+      <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
+      <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
    
 
-    <div className="block w-full overflow-x-auto">
+      <div className="block w-full overflow-x-auto">
       {/* <table className="items-center bg-transparent w-full border-collapse ">
         <thead>
           <tr>
