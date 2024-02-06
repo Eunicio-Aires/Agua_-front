12dc80ce73cc715de161e57689bea85ff3294dde
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-06 19:17:04 +0200

    alterasoo

--- components/Procurarc.tsx
+++ components/Procurarc.tsx
@@ -55,18 +55,19 @@ export  function Procurarc(props:Mes){
       }
       return (
  
-        <div className="">
+        <div className="max-w-md mx-auto">
+  <form onSubmit={handleSubmit} className="flex flex-col space-y-3 sm:flex-row sm:space-y-0 sm:space-x-3">
+    <input type="text" required className="w-full sm:w-64 rounded-md border bg-gray-50 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:cursor-not-allowed disabled:opacity-50" placeholder="Codigo do cliente" id="codigo" name="codigo" />
+    <button type="submit" className="w-full sm:w-auto rounded-md border border-blue-500 bg-blue-500 py-2 px-6 text-white transition hover:border-blue-600 hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:border-blue-500 disabled:hover:bg-blue-500">Procurar</button>
+  </form>
 
-        {/* <div className="flex flex-col items-center justify-center
-         space-y-6 text-center"> */}
-            
-            <form onSubmit={handleSubmit} className=" max-w-md flex-col  space-y-3 sm:flex-row sm:space-y-0 sm:space-x-3"><input type="text" required className="w-full rounded-md border bg-gray-50 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:cursor-not-allowed disabled:opacity-50" placeholder="Codigo do cliente" id="codigo"name="codigo"  />
-            <button type="submit" className="w-full rounded-md border border-blue-500 bg-blue-500 py-2 px-6 text-white transition hover:border-blue-600 hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:border-blue-500 disabled:hover:bg-blue-500 sm:max-w-max">Procurar</button></form>
-            {/* </div> */}
+  <RegisFatura  resulte={resultId} _id={props._id} />
+</div>
+
         
-        <RegisFatura  resulte={resultId} _id={props._id} />
+        
 
-     </div>
+     
       )
 }
 
