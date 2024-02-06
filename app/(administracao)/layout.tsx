f7761a22b746da28e314cf3d75442ffed29380a0
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-06 19:46:08 +0200

    agyrytdy

--- app/(administracao)/layout.tsx
+++ app/(administracao)/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 
           <div>
           
-              <div className="navbar bg-base-100">
+              {/* <div className="navbar bg-base-100">
                 <div className="navbar-start">
                   <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost btn-circle">
@@ -36,7 +36,7 @@ export default function RootLayout({
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                   </button>
                 </div>
-              </div>
+              </div> */}
 
           <div className="flex">
             <div className="flexed w-14 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between">
