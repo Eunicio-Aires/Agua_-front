4a467174b70f2d0c4d5570b8581d39cf473c2241
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-06 21:21:24 +0200

    rtrewq

--- components/Formda.tsx
+++ components/Formda.tsx
@@ -37,7 +37,7 @@ export function Formda(ligacoes:Ligacao){
       <div className="mt-8 flex items-center justify-center">
       <form onSubmit={handleSubmit} className='bg-white rounded-2xl border shadow-x1 p-4 sm:p-10 max-w-md '>
         <label htmlFor="leitura" className="sr-only">Leitura</label>
-        <input type="text" id="leitura" name="leitura" required className="input input-bordered input-info w-full max-w-xs sm:max-w-none" placeholder="Leitura" />
+        <input type="text" id="leitura" name="leitura" required className="input input-bordered input-info  max-w-xs sm:max-w-none" placeholder="Leitura" />
         <button className="btn btn-outline btn-info ml-1" type="submit">Submeter Leitura</button>
       </form>
     </div>
