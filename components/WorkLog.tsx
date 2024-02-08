90cd50404ff2a8e38dfc8fc3953913ef21726ea0
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-08 13:48:59 +0200

    tudo comentado

--- components/WorkLog.tsx
+++ components/WorkLog.tsx
@@ -7,41 +7,41 @@ import React, { cookies } from 'next/headers'
 
 
 export  function WorkLog(){
-    const router = useRouter();
-    const handleSubmi = async (event: SyntheticEvent) => {
-      event.preventDefault();
+    // const router = useRouter();
+    // const handleSubmi = async (event: SyntheticEvent) => {
+    //   event.preventDefault();
   
       
-      const data = {
-        email: (event.target as HTMLFormElement).email.value,
-        password: (event.target as HTMLFormElement).password.value,
+    //   const data = {
+    //     email: (event.target as HTMLFormElement).email.value,
+    //     password: (event.target as HTMLFormElement).password.value,
     
-      };
-      const JSONdata = JSON.stringify(data);
-      const endpoint = `https://agua-p.vercel.app/adm/loginSadmin`;
-      const options: RequestInit = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json', 
-        },
-        body: JSONdata,
-      };
+    //   };
+    //   const JSONdata = JSON.stringify(data);
+    //   const endpoint = `https://agua-p.vercel.app/adm/loginSadmin`;
+    //   const options: RequestInit = {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json', 
+    //     },
+    //     body: JSONdata,
+    //   };
       
       
     
-      try{
-        const response = await fetch(endpoint, options);
-      const result = await response.json();
+    //   try{
+    //     const response = await fetch(endpoint, options);
+    //     const result = await response.json();
     //   const cookieStore = cookies()
     //   const token = cookieStore.get('token')
     //   alert(token)
     //   const id = await result._id
     //   router.push(`pagamentos/${id}`)
-      }catch(error){
-        alert(`Verifique o seu Codigo, Codigo invalido`);
-      }
+    //   }catch(error){
+    //     alert(`Verifique o seu Codigo, Codigo invalido`);
+    //   }
   
-    }
+    // }
 
     return (
         <>
@@ -54,7 +54,7 @@ export  function WorkLog(){
                 <p className="text-gray-500 dark:text-gray-400">Navegacao segura </p>
             </div>
             <div className="m-7">
-                <form onSubmit={handleSubmi}>
+                <form>
                     <div className="mb-6">
                         <label  className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Email </label>
                         <input type="email" name="email" id="email" placeholder="you@company.com" className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
