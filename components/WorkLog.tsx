5a14c73007abc66abe82b8f33eecb431f110487c
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-08 03:11:39 +0200

    editado

--- components/WorkLog.tsx
+++ components/WorkLog.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { SyntheticEvent } from "react";
-import { cookies } from 'next/headers'
+import React, { cookies } from 'next/headers'
 
 
 
