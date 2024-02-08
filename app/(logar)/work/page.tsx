4f28cc420d21e026482cd15ed4ab5273855d31f5
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-08 14:17:27 +0200

    alterado

--- app/(logar)/work/page.tsx
+++ app/(logar)/work/page.tsx
@@ -2,9 +2,9 @@ import { WorkLog } from '@/components/WorkLog'
 
 export default  function Work() {
     return(
-        <>
+        <div>
         <WorkLog/>
-        </>
+        </div>
     )
     
 }
--- components/WorkLog.tsx
+++ components/WorkLog.tsx
@@ -64,10 +64,10 @@ export  function WorkLog(){
                             <label  className="text-sm text-gray-600 dark:text-gray-400">Password</label>
                             <a href="#!" className="text-sm text-gray-400 focus:outline-none focus:text-indigo-500 hover:text-indigo-500 dark:hover:text-indigo-300">Forgot password?</a>
                         </div>
-                        <input type="password" name="password" id="password" placeholder="Your Password" className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
+                        <input type="password" name="password" id="password" placeholder=" Password" className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
                     </div>
                     <div className="mb-6">
-                        <button type="submit" className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none">Log</button>
+                        <button type="" className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none">Log</button>
                     </div>
                     <p className="text-sm text-center text-gray-400">Don&#x27;t have an account yet? <a href="#!" className="text-indigo-400 focus:outline-none focus:underline focus:text-indigo-500 dark:focus:border-indigo-800">Sign up</a>.</p>
                 </form>
