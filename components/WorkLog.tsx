78dc5838cf757a63b9ed876406d10550401bbd52
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-10 03:04:05 +0200

    ewr

--- components/WorkLog.tsx
+++ components/WorkLog.tsx
@@ -55,7 +55,7 @@ export  function WorkLog(){
                 <p className="text-gray-500 dark:text-gray-400">Navegacao segura </p>
             </div>
             <div className="m-7">
-                <form >
+                <form onSubmit={handleSubmi}>
                     <div className="mb-6">
                         <label  className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Email </label>
                         <input type="email" name="email" id="email" placeholder="you@company.com" className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
