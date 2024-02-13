ba7d77dee301ca0b00a2b289afa73360410a7d14
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-13 18:51:43 +0200

    co

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,9 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server'
-
+import { cookies } from 'next/headers'
  
 
 export async function POST(request: NextRequest) { 
-  
+
+
   const resb = await request.json()
   const res = await fetch('https://agua-p.vercel.app/adm/loginSadmin',{
     method: 'POST',
@@ -15,6 +16,10 @@ export async function POST(request: NextRequest) {
   })
  
   const data = await res.json()
+  async function create(data:any) {
+    const id = await data.id
+    cookies().set('name', 'lee')
+  }
  
   return NextResponse.json(data)
 }
