f7fb78640eac73515615b050e8795e83110193f4
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-13 18:24:44 +0200

    retirado id

--- components/WorkLog.tsx
+++ components/WorkLog.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 
 
 export  function WorkLog(){
-   const allCookies = cookies().getAll()
+  
    
     const handleSubmi = async (event: SyntheticEvent) => {
       event.preventDefault();
@@ -38,7 +38,7 @@ export  function WorkLog(){
         // cookies().set('token', token)
         
       
-     console.log('cookies', allCookies)
+     console.log(id)
       alert("submetido")
       // const id = await result._id
       // router.push(`pagamentos/${id}`)
