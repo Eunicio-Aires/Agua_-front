4e88e90262f30244069a0cb1b237ccc44b773600
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-16 23:16:39 +0200

    token no console

--- app/(administracao)/clientes/[id]/page.tsx
+++ app/(administracao)/clientes/[id]/page.tsx
@@ -1,5 +1,3 @@
-
-
 import Link from "next/link"
 import React, { SyntheticEvent } from 'react';
 import { Movitel } from '@/components/pagamento/Movitel';
--- app/(administracao)/logadoAdmin/[id]/page.tsx
+++ app/(administracao)/logadoAdmin/[id]/page.tsx
@@ -1,6 +1,10 @@
+import { cookies } from "next/headers"
 import Link from "next/link"
 
 export default async function ClienteId({ params }: { params: { id: string } }){
+    const cookie = cookies()
+    const toke = cookie.get('token')
+    console.log(toke)
     const compan = await  fetch(`https://agua-front.vercel.app/api/adminComp/${params.id}`,{ cache: 'no-store'}).then((res) => res.json())
     const nome  = await compan.nome
     const apelido  = await compan.apelido
@@ -8,6 +12,8 @@ export default async function ClienteId({ params }: { params: { id: string } }){
     const comp = await compan.company
     return(
         <>
+        
+
             <div>{nome} {apelido}</div>
             {comp.map((companes:any)=>(
                 <div  key={companes._id}>
