9e95acf00bb97b333bb680c379e23113e6ef0dd9
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-16 03:34:19 +0200

    alterado

--- app/(administracao)/logadoAdmin/[id]/page.tsx
+++ app/(administracao)/logadoAdmin/[id]/page.tsx
@@ -1,11 +1,24 @@
+import Link from "next/link"
 
 export default async function ClienteId({ params }: { params: { id: string } }){
     const compan = await  fetch(`https://agua-front.vercel.app/api/adminComp/${params.id}`,{ cache: 'no-store'}).then((res) => res.json())
     const nome  = await compan.nome
     const apelido  = await compan.apelido
+    const id  = await compan._id
+    const comp = await compan.company
     return(
         <>
             <div>{nome} {apelido}</div>
+            {comp.map((companes:any)=>(
+                <div  key={companes._id}>
+                    <Link href='/'>
+                        <div>{companes.nome}</div>
+                        <div>{companes.clientes.length}</div>
+                    </Link>
+                </div>
+                        
+                    ))}
+
         </>
     )
 
