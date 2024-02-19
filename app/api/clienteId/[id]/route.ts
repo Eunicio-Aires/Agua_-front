1d0404ab8a5234af07f59593295dcc7b749c118b
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-19 03:53:20 +0200

    alterat

--- app/api/clienteId/[id]/route.ts
+++ app/api/clienteId/[id]/route.ts
@@ -9,7 +9,7 @@ export async function GET(request: Request,
 
     const tokenCl = getSession()
     const headers:any = {
-      'Authorization': tokenCl,
+      'authorization': tokenCl,
       
     }
 
