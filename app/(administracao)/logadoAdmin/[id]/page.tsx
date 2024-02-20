5605bfcd5e6ac5a83c0f18092ca0f7963d52c280
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-20 21:07:23 +0200

    removido o id

--- app/(administracao)/logadoAdmin/[id]/page.tsx
+++ app/(administracao)/logadoAdmin/[id]/page.tsx
@@ -20,7 +20,7 @@ export default async function ClienteId({ params }: {params:{id: string }}){
          <div>{nome} {apelido}</div>
             {comp.map((companes:any)=>(
                 <div  key={companes._id}>
-                    <Link href={`/inicial/${idc}`}>
+                    <Link href={`/inicial/${companes._id}`}>
                         <div>{companes.nome}</div>
                         <div>{companes.clientes.length}</div>
                     </Link>
