5e91b4eb6dab9ab91effb45724187fce53525612
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-22 18:26:48 +0200

    cooki no server ipa

--- app/(administracao)/inicial/[id]/page.tsx
+++ app/(administracao)/inicial/[id]/page.tsx
@@ -13,8 +13,7 @@ import { cookies } from 'next/headers'
 export default async function ClientComp({ params }: { params: { id: string } }){
   
     const dasos = await  fetch(`https://agua-front.vercel.app/api/compoCliente/${params.id}`,{ cache: 'no-store'}).then((res) => res.json())
-    const idComp = await `${params.id}`
-    cookies().set('idcomp' , idComp )    
+    
     const responseC = await dasos.clientes
     const numero  = await responseC.filter((cliente:any) => cliente.estado === "Activo").length
     const ativos  = await responseC.filter((cliente:any) => cliente.estado === "Activo")
--- app/api/compoCliente/[id]/route.ts
+++ app/api/compoCliente/[id]/route.ts
@@ -1,12 +1,12 @@
 import { NextRequest,NextResponse } from 'next/server';
-
+import { cookies } from 'next/headers';
 
 export async function GET(
     request: Request,
     { params }: { params: { id: string } }
   ) {
     const id = params.id 
-
+    cookies().set('idcomp' , id )  
     const res = await fetch(`https://agua-p.vercel.app/adm/componeclient/${id}`,{cache:"no-cache"})
     const product = await res.json()
     return NextResponse.json( product )
