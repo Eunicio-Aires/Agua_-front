64c9d12a9eede9def948286bfcb6114078601dfd
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-23 01:59:15 +0200

    wheqlfkbklasbdf

--- app/api/compoCliente/[id]/route.ts
+++ app/api/compoCliente/[id]/route.ts
@@ -11,7 +11,8 @@ export async function GET(
     const product = await res.json()
     const idcomp = await product.paramComp
     const dados = await product.compan
-    cookies().set('idcompy' , idcomp ) 
+    cookies().set('idcompy' , 'klsdflknsdfgsdfgklnb' ) 
+    // cookies().set('token' , token )
 
     return NextResponse.json( dados )
   }
