9ef11249b7723b8d2db6b1f354111db59ac23d88
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-23 02:20:26 +0200

    compo alterado

--- app/api/compoCliente/[id]/route.ts
+++ app/api/compoCliente/[id]/route.ts
@@ -11,7 +11,7 @@ export async function GET(
     const product = await res.json()
     const idcomp = await product.paramComp
     const dados = await product.compan
-    cookies().set('idcompy' , 'klsdflknsdfgsdfgklnb' ) 
+    cookies().set('idco' , id ) 
     // cookies().set('token' , token )
 
     return NextResponse.json( dados )
