26b6be9deab5f6d09616676b7d7f828ae4aa0919
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-24 08:47:13 +0200

    eeeeeeeeedfhj

--- app/api/initacook/[id]/route.ts
+++ app/api/initacook/[id]/route.ts
@@ -2,13 +2,13 @@ import { NextRequest, NextResponse } from 'next/server'
 import { headers } from 'next/headers'
 import { cookies } from 'next/headers'
 
-export async function POST(request:NextRequest,{params}:any) {
-    const ids = await params._id;
+export async function POST(request:NextRequest,{ params }: { params: { id: string } }) {
+    const ids = await params.id;
    
     
   //   const body = await request.json();
     try {
-      const response = await fetch(`https://agua-p.vercel.app/adm/fontcook/${params._id}`,{
+      const response = await fetch(`https://agua-p.vercel.app/adm/fontcook/${ids}`,{
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -16,9 +16,9 @@ export async function POST(request:NextRequest,{params}:any) {
       //   body: JSON.stringify(body),
       });
       const data = await response.json();
-      const id   = await data
+      const id   = await data.paramComp
       cookies().set('idCompan' , id )
-      return NextResponse.json(data); 
+      return NextResponse.json(id); 
     } catch (error) {
       return NextResponse.json({ error: 'An error occurred' }, { status: 500 });
     }
