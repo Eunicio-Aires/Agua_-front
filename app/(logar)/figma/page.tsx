cefe54c7e39442375144304e6f1b595cdee4cf8e
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-02-26 16:24:39 +0200

    khgvldknsv

--- app/(logar)/figma/page.tsx
+++ app/(logar)/figma/page.tsx
@@ -1,20 +1,21 @@
 
 export default function Figma(){
 
-    return(
-        <div className="w-[390px] rounded-3xs bg-white h-[844px] overflow-hidden text-center text-3xs text-lightseagreen font-poppins"> 
-      <div className="absolute top-[391px] left-[76px] text-[32px] [text-shadow:0.5px_0_0_#04adbf,_0_0.5px_0_#04adbf,_-0.5px_0_0_#04adbf,_0_-0.5px_0_#04adbf]">
+  return(
+    <>
+      <div className="w-[390px] rounded-3xs bg-white h-[844px] overflow-hidden text-center text-3xs text-lightseagreen font-poppins">
+      <img
+        className="absolute top-[454px] left-[3px] w-[274.2px] h-[389.7px] object-contain"
+        alt=""
+        src="/ellipse-2.svg"
+      />
+      <div className="absolute top-[391px] left-[76px] text-13xl [text-shadow:0.5px_0_0_#04adbf,_0_0.5px_0_#04adbf,_-0.5px_0_0_#04adbf,_0_-0.5px_0_#04adbf]">
         Fatura na mão
       </div>
       <img
         className="absolute top-[0px] left-[3px] w-[386px] h-[290px] object-cover"
         alt=""
         src="/hands4903050-1280e16238370891651090x613removebgpreview-1@2x.png"
-      />
-      <img
-        className="absolute top-[-73px] left-[97px] w-[469px] h-[435px]"
-        alt=""
-        src="/ellipse-1.svg"
       />
       <div className="absolute top-[823px] left-[147px] text-darkcyan-100">
         <span>{`designed by `}</span>
@@ -24,7 +25,7 @@ export default function Figma(){
         <p className="m-0">Faça consulta das suas faturas e</p>
         <p className="m-0"> pague sem sair de casa</p>
       </div>
-      <div className="absolute top-[588.5px] left-[37.5px] box-border w-[313px] h-px border-t-[1px] border-solid border-black" />
+      <div className="absolute top-[588.5px] left-[37.5px] box-border w-[313px] h-px border-t-[1px] border-solid border-darkcyan-200" />
       <div className="absolute top-[618px] left-[33px] rounded-3xs bg-lightseagreen w-[325px] h-[38px]" />
       <div className="absolute top-[622px] left-[159px] text-[20px] text-white [-webkit-text-stroke:0.3px_#fff]">
         Entrar
@@ -41,6 +42,22 @@ export default function Figma(){
         alt=""
         src="/person@2x.png"
       />
+      <img
+        className="absolute top-[1px] left-[104px] w-[285px] h-[390px]"
+        alt=""
+        src="/ellipse-2.svg"
+      />
+      <img
+        className="absolute top-[674px] left-[272px] w-[45px] h-7 object-cover"
+        alt=""
+        src="/ringer-volume@2x.png"
+      />
+      <img
+        className="absolute top-[674px] left-[317px] w-[34px] h-7 object-cover"
+        alt=""
+        src="/mail-account@2x.png"
+      />
     </div>
-    )
+    </>
+  )
 }
