52e1ca393ac2b28ac1fcdde96f6ea79433825b23
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-04 10:47:44 +0200

    tyfghc

--- app/api/mes/[id]/route.ts
+++ app/api/mes/[id]/route.ts
@@ -7,7 +7,7 @@ export async function GET(
     const idCom = getCompan()
     const id = params.id 
 
-    const res = await fetch(`https://agua-p.vercel.app/adm/mesesGetbtcomp/${idCom}`,{cache: 'no-store'})
+    const res = await fetch(`https://agua-p.vercel.app/adm/mesesGetbtcomp/65a956bb8f3b0f53bed8bf8d`,{cache: 'no-store'})
     const clientes = await res.json()
     return NextResponse.json( clientes )
   }
