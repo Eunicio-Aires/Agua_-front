7f068681914beeff50aeba9f675f073fc25a9108
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-04 07:36:57 +0200

    wedefvds

--- app/(administracao)/faturar/[faturacao]/page.tsx
+++ app/(administracao)/faturar/[faturacao]/page.tsx
@@ -26,9 +26,6 @@ export default async function Page({ params }:{ params:{faturacao:Mes}}) {
             {/* <h1 className="text-center text-xl font-bold tracking-normal sm:text-4xl lg:text-4xl mt-1">{posts.mes.mes}</h1>  */}
             
         <Procurarc _id={mes}/>
-
-           
-
         </div>
             
     ) 
