7c256676f309c7e8a9bc13373a7676b71e201de1
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-04 17:29:45 +0200

    dtghsdfhdfhh

--- app/(administracao)/faturar/page.tsx
+++ app/(administracao)/faturar/page.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 export default async function Faturar(){
   const idf = '1234'
   const response = await fetch(`https://agua-front.vercel.app/api/mes/${idf}`,{cache:"no-cache"}).then((res) => res.json());
+  console.log(response)
   return(
     <div className="">
     
@@ -45,7 +46,7 @@ export default async function Faturar(){
         </thead>
 
         <tbody>
-        {response.mes.map((mes:any)=>(
+        {/* {response.mes.map((mes:any)=>(
           <tr key= {mes._id} className="hover-gray-light transition">
             <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
             { mes.mes }
@@ -62,7 +63,7 @@ export default async function Faturar(){
           >Faturar</Link>
             </td>
           </tr>
-           ))} 
+           ))}  */}
         </tbody>
 
       </table>
