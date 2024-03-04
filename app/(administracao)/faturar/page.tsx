3e8dcc8130525d0ef7c4d42f4b8ae47fc3cd874f
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-04 10:41:54 +0200

    ashsfh

--- app/(administracao)/faturar/page.tsx
+++ app/(administracao)/faturar/page.tsx
@@ -1,8 +1,9 @@
-
+import { getCompan } from '@/lib/cookiesConf'
 import { Meses }  from '@/components/Meses'
 
 export default function Faturar(){
-
+  const idCom = getCompan()
+  console.log(idCom)
   return(
     <div className="">
     
