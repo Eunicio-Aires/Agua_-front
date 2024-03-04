770ced0ec1721cf0c9499c80ed1f30c3d7331c99
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-04 07:39:50 +0200

    weffad

--- components/UserItems.tsx
+++ components/UserItems.tsx
@@ -4,17 +4,17 @@ import { getapelido } from "@/lib/cookiesConf";
 import { getemail } from "@/lib/cookiesConf"
 export default function UserItems(){
 
-    const nome = getnome();
-    const apelido = getapelido();
-    const email = getemail()
+    // const nome = getnome();
+    // const apelido = getapelido();
+    // const email = getemail()
 
     return <div className='flex items-center justify-between gap-2 border rounded-[16px] p-2'> 
         <div className='avatar rounded-full min-h-10 min-w-10 bg-emerald-400 text-white font-[700] flex items-center justify-center'>
             <p>EA</p>
         </div>
         <div>
-            <p className=" text-[16px] font-bold">{nome} {apelido}</p>
-            <p className="text-[12px] text-neutral-500">{email}</p>
+            <p className=" text-[16px] font-bold">Eunicio Aires</p>
+            <p className="text-[12px] text-neutral-500">euniciojoseaires@gmail.com</p>
         </div>
     </div>
 }
