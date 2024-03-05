62e02f0615e5bc64f3407b123841fdc5e06d8217
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-05 13:31:10 +0200

    iuuyfjkvhk

--- components/WorkLog.tsx
+++ components/WorkLog.tsx
@@ -3,13 +3,15 @@ import { useRouter } from "next/navigation";
 import React,  { SyntheticEvent } from "react";
 import { cookies } from 'next/headers'
 import Link from "next/link";
-
-
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 export  function WorkLog(){
-  // const cookieStore = cookies()
+  const notify = () => toast.success("Solicitado com sucesso, verifica Seu M-pesa",{
+    theme: "colored"
+ });
+
   const router = useRouter()
    
     const handleSubmi = async (event: SyntheticEvent) => {
@@ -31,13 +33,17 @@ export  function WorkLog(){
       
       const response = await fetch(endpoint, options);
         const result = await response.json(); 
-        const token =await result.token
-        const id =await result.id
+        
       //   const idCanp = await result.id
     
       try{
- 
+        if(result.status === 200){
+        const token =await result.token
+        const id =await result.id
       router.push(`/logadoAdmin/${id}`)
+        }else{
+          notify()
+        }
       }catch(error){
         alert(`Verifique o seu Codigo, Codigo invalido`);
       }
@@ -52,7 +58,7 @@ export  function WorkLog(){
         <div className="max-w-md mx-auto my-10">
             <div className="text-center">
                 <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">Entrar</h1>
-                <p className="text-gray-500 dark:text-gray-400">Navegacao segura </p>
+                <p className="text-gray-500 dark:text-gray-400">Navegacao Segura </p>
             </div>
             <div className="m-7">
                 <form onSubmit={handleSubmi}>
@@ -63,14 +69,14 @@ export  function WorkLog(){
                     <div className="mb-6">
                         <div className="flex justify-between mb-2">
                             <label  className="text-sm text-gray-600 dark:text-gray-400">Password</label>
-                            <a href="#!" className="text-sm text-gray-400 focus:outline-none focus:text-indigo-500 hover:text-indigo-500 dark:hover:text-indigo-300">Forgot password?</a>
+                            <a href="#!" className="text-sm text-gray-400 focus:outline-none focus:text-indigo-500 hover:text-indigo-500 dark:hover:text-indigo-300"></a>
                         </div>
                         <input type="password" name="password" id="password" placeholder=" Password" className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
                     </div>
                     <div className="mb-6">
                         <button type="submit" className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none">Log</button>
                     </div>
-                    <p className="text-sm text-center text-gray-400">Don&#x27;t have an account yet? <a href="#!" className="text-indigo-400 focus:outline-none focus:underline focus:text-indigo-500 dark:focus:border-indigo-800">Sign up</a>.</p>
+                    <p className="text-sm text-center text-gray-400"> <a href="#!" className="text-indigo-400 focus:outline-none focus:underline focus:text-indigo-500 dark:focus:border-indigo-800">Contact</a>.</p>
                 </form>
             </div>
         </div>
@@ -82,7 +88,7 @@ export  function WorkLog(){
 <div className="fixed bottom-5 w-full text-center text-gray-400">
 	Crafted with ♡ by <Link  href=""></Link> <p className="text-blue-500">E.Aires</p>
 </div>
-        
+        <ToastContainer/> 
         </>
     )
 }
