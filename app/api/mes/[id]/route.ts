09043e30eb004ea5f2e69c145d7ff3d56be99189
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-08 01:33:25 +0200

    mes esquec

--- app/api/mes/[id]/route.ts
+++ app/api/mes/[id]/route.ts
@@ -6,8 +6,12 @@ export async function GET(
   ) {
     const idCom = getCompan()
     const id = params.id 
-
-    const res = await fetch(`https://agua-p.vercel.app/adm/mesesGetbtcomp/65a956bb8f3b0f53bed8bf8d`,{cache: 'no-store'})
+    const token = await `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YWE3NzRjMGUyODA5OTAwMmYxNThlMyIsImlhdCI6MTcwOTgyMTA3MSwiZXhwIjoxNzA5OTA3NDcxfQ.oeKHAXmxlcfCFwSyBc7A0HxlSrH2vrjfgaZj-mKk3-8`
+    const res = await fetch(`https://agua-p.vercel.app/adm/mesesGetbtcomp/65a956bb8f3b0f53bed8bf8d`,{
+      headers: {
+        'Authorization' :`${token }`,
+      }
+    })
     const clientes = await res.json()
     return NextResponse.json( clientes )
   }
