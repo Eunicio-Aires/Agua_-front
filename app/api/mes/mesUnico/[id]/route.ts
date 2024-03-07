9e5dad13b3778cf4401b21fcc4887f24f6b1c5f9
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-08 01:27:02 +0200

    fghxh

---
+++ app/api/mes/mesUnico/[id]/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse, NextRequest } from "next/server"
+import { getCompan } from "@/lib/cookiesConf"
+export async function GET(
+    request: NextRequest,
+    { params }: { params: { id: string } }
+  ) {
+    const idCom = getCompan()
+    const id = params.id 
+    const token = await `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YWE3NzRjMGUyODA5OTAwMmYxNThlMyIsImlhdCI6MTcwOTgyMTA3MSwiZXhwIjoxNzA5OTA3NDcxfQ.oeKHAXmxlcfCFwSyBc7A0HxlSrH2vrjfgaZj-mKk3-8`
+    const res = await fetch(`https://agua-p.vercel.app/adm/mesunic/65a956bb8f3b0f53bed8bf8d`,{
+        headers: {
+          'Authorization' :`${token}`,
+        }
+      })
+    const clientes = await res.json()
+    return NextResponse.json( clientes )
+  }
