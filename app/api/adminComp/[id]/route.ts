89ef8339e4f6dc5f05635f0aa8f1d31a3de3015a
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-07 16:41:40 +0200

    fhgfdh

--- app/api/adminComp/[id]/route.ts
+++ app/api/adminComp/[id]/route.ts
@@ -7,7 +7,12 @@ export async function GET(
   ) {
     const id = params.id 
 
-    const res = await fetch(`https://agua-p.vercel.app/adm/adminComp/${id}`,{cache: 'no-store'})
+    const res = await fetch(`https://agua-p.vercel.app/adm/adminComp/${id}`,{
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YWE3NzRjMGUyODA5OTAwMmYxNThlMyIsImlhdCI6MTcwOTgyMTA3MSwiZXhwIjoxNzA5OTA3NDcxfQ.oeKHAXmxlcfCFwSyBc7A0HxlSrH2vrjfgaZj-mKk3-8',
+      },
+    })
     const clientes = await res.json()
     return NextResponse.json( clientes )
   }
---
+++ components/clientes/Procurar.tsx
@@ -0,0 +1,65 @@
+'use client' 
+import React, {useState, SyntheticEvent } from 'react';
+// import { RegisFatura } from './RegiFatura';
+// import { Detalhes } from './Detalhes';
+// import { revalidateTag } from 'next/cache';
+
+interface Faturas{
+  _id:String;
+  mes:String;
+  estado:String;
+  valor:Number
+}
+
+interface Result {
+  _id:string;
+  faturas:Faturas[]
+
+}
+
+interface Mes {
+  _id:String;
+  
+}
+export  function Procura(){
+  const [resultId, setResultId] = useState<any>(); 
+    const handleSubmit = async (event:SyntheticEvent) => {
+        
+        event.preventDefault()
+   
+        const data = {
+          codigo:(event.target as HTMLFormElement).codigo.value,
+
+        }
+        const JSONdata = JSON.stringify(data)
+     
+        const endpoint = 'https://agua-front.vercel.app/api/procurar'
+ 
+        const options = {
+         
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSONdata,
+        }
+     
+        const response = await fetch(endpoint, options)
+        const result:Result  = await response.json()
+        
+        setResultId(result);
+ 
+      }
+      return (
+ 
+        <div className="">
+        <form onSubmit={handleSubmit} className="flex flex-col space-y-3 sm:flex-row sm:space-y-0 sm:space-x-3">
+          <input type="text" required className=" sm:w-64 rounded-md border bg-gray-50 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:cursor-not-allowed disabled:opacity-50" placeholder="Codigo do cliente" id="codigo" name="codigo" />
+          <button type="submit" className=" sm:w-auto rounded-md border border-blue-500 bg-blue-500 py-2 px-6 text-white transition hover:border-blue-600 hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:border-blue-500 disabled:hover:bg-blue-500">Procurar</button>
+        </form>
+      
+        {/* <RegisFatura resulte={resultId} _id={props._id} /> */}
+
+      </div>
+      )
+}
