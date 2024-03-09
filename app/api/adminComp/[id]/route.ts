f42fddbeeb8c1719eb759c1b8eb44fa0945ef470
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-10 00:53:29 +0200

    honestly

--- app/api/adminComp/[id]/route.ts
+++ app/api/adminComp/[id]/route.ts
@@ -1,13 +1,19 @@
 import { NextRequest,NextResponse } from 'next/server';
-
+import { getSession } from '@/lib/cookiesConf';
 
 export async function GET(
     request: Request,
     { params }: { params: { id: string } }
   ) {
     const id = params.id 
+    const token = await getSession()
 
-    const res = await fetch(`https://agua-p.vercel.app/adm/adminComp/${id}`,)
+    const res = await fetch(`https://agua-p.vercel.app/adm/adminComp/${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization' : `${token}`,
+      },
+    })
     const clientes = await res.json()
     return NextResponse.json( clientes )
   }
