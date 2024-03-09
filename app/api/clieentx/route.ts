362f34ea8e26d3192440ee8246be8603dbbc6f68
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-10 01:22:24 +0200

    dfghfh

--- app/api/clieentx/route.ts
+++ app/api/clieentx/route.ts
@@ -1,10 +1,10 @@
  
 import { NextResponse } from 'next/server'
 
-export async function GET(request: Request) {
-  const res = await fetch('https://agua-p.vercel.app/adm/allAreas',{cache:"no-cache"})
-  const respo = await res.json();
+// export async function GET(request: Request) {
+//   const res = await fetch('https://agua-p.vercel.app/adm/allAreas',{cache:"no-cache"})
+//   const respo = await res.json();
 
-  return NextResponse.json(respo)
-}
+//   return NextResponse.json(respo)
+// }
 
