e0cd1af6da8e8995362999fd47901ac35c236430
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-10 13:25:32 +0200

    drtuydrtu

--- app/(administracao)/inicial/[id]/page.tsx
+++ app/(administracao)/inicial/[id]/page.tsx
@@ -16,8 +16,9 @@ import Clientes from "@/components/clientes/Clientes";
 
 
 
+
 export default async function ClientComp({ params }: { params: { id: string } }){
-  
+  const token = await getSession()
   const baseUrl = process.env.NEXT_LOCAL_BASE_URL;
     const dasos = await  fetch(`${baseUrl}/api/compoCliente/${params.id}`,{ cache: 'no-store'}).then((res) => res.json())
    
@@ -29,33 +30,33 @@ export default async function ClientComp({ params }: { params: { id: string } })
     const seativo = await responseC.filter((cliente: any) => cliente.estado !== "Activo");
     
 
-    return(
+return (
 
       <main className=" min-h-screen p-4 mb-4">
 
-
-{/* <section className="flex flex-wrap justify-center">
-                  <div className="w-full md:w-1/3 p-4">
-                      <div className="bg-white rounded-lg shadow-md p-6">
-                      <h2 className="text-xl font-bold mb-2">{numero} </h2>
-                      <p>Conteúdo do Card 1...</p>
-                      </div>
-                  </div>
-                  <div className="w-full md:w-1/3 p-4">
-                      <div className="bg-white rounded-lg shadow-md p-6">
-                      <h2 className="text-xl font-bold mb-2">Card 2</h2>
-                      <p>Conteúdo do Card 2...</p>
-                      </div>
-                  </div>
-                  <div className="w-full md:w-1/3 p-4">
-                      <div className="bg-white rounded-lg shadow-md p-6">
-                      <h2 className="text-xl font-bold mb-2">Card 3</h2>
-                      <p>Conteúdo do Card 3...</p>
-                      </div>
-                  </div>
-    
-   
-   </section> */}
+    <div>{token}</div>
+      <section className="flex flex-wrap justify-center">
+                        <div className="w-full md:w-1/3 p-4">
+                            <div className="bg-white rounded-lg shadow-md p-6">
+                            <h2 className="text-xl font-bold mb-2">{numero} </h2>
+                            <p>Conteúdo do Card 1...</p>
+                            </div>
+                        </div>
+                        <div className="w-full md:w-1/3 p-4">
+                            <div className="bg-white rounded-lg shadow-md p-6">
+                            <h2 className="text-xl font-bold mb-2">Card 2</h2>
+                            <p>Conteúdo do Card 2...</p>
+                            </div>
+                        </div>
+                        <div className="w-full md:w-1/3 p-4">
+                            <div className="bg-white rounded-lg shadow-md p-6">
+                            <h2 className="text-xl font-bold mb-2">Card 3</h2>
+                            <p>Conteúdo do Card 3...</p>
+                            </div>
+                        </div>
+      
+      
+         </section>
       <Registar/>
       <Clientes responseC={responseC}/>
 
@@ -65,80 +66,6 @@ export default async function ClientComp({ params }: { params: { id: string } })
 
 
 
-
-
-      
-  //       < className="text-gray-600 body-font">
-       
-  // <input type="checkbox" id="my_modal_7" className="modal-toggle" />
-  // <div className="modal" role="dialog">
-  //   <div className="modal-box">
-  //     <h3 className="text-lg font-bold">Hello!</h3>
-  //     <p className="py-4">This modal works with a hidden checkbox!</p>
-      
-  //   </div>
-  //   <label className="modal-backdrop" htmlFor="my_modal_7">Close</label>
-  // </div>
-  
-  // <section className="flex flex-wrap justify-center">
-  //                 <div className="w-full md:w-1/3 p-4">
-  //                     <div className="bg-white rounded-lg shadow-md p-6">
-  //                     <h2 className="text-xl font-bold mb-2">{numero} </h2>
-  //                     <p>Conteúdo do Card 1...</p>
-  //                     </div>
-  //                 </div>
-  //                 <div className="w-full md:w-1/3 p-4">
-  //                     <div className="bg-white rounded-lg shadow-md p-6">
-  //                     <h2 className="text-xl font-bold mb-2">Card 2</h2>
-  //                     <p>Conteúdo do Card 2...</p>
-  //                     </div>
-  //                 </div>
-  //                 <div className="w-full md:w-1/3 p-4">
-  //                     <div className="bg-white rounded-lg shadow-md p-6">
-  //                     <h2 className="text-xl font-bold mb-2">Card 3</h2>
-  //                     <p>Conteúdo do Card 3...</p>
-  //                     </div>
-  //                 </div>
-    
-   
-  //  </section>
-  // <div className=" grid lg:grid-cols-5 gap-4 p-4 bg-gray-200"> 
-  
-  //             <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg">
-  //               <div className="flex flex-col w-full pb-4">
-  //                 <p className="text-2xl font-bold">{ativos.length}</p>
-  //                 <p className="text-gray-600">Clientes ativos  </p>
-  //               </div>
-  //               <p className="bg-green-200 flex justify-center items-center p-2 rounded-lg">
-  //                 <span className='text-green-700 text-lg'>{ativos.length}</span>
-  //               </p>
-  //             </div>
-              
-  //             <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg">
-  //               <div className="flex flex-col w-full pb-4">
-  //               <p className="text-2xl font-bold">{seativo.length}</p>
-  //                 <p className="text-gray-600">Desativados</p>
-  //               </div>
-  //               <p className="bg-red-400 flex justify-center items-center p-2 rounded-lg">
-  //                 <span className='text-green-700 text-lg'>{seativo.length}</span>
-  //               </p>
-  //             </div>
-  //             <div className="bg-white flex justify-btween w-full p-4 rounded">
-  //               <div className=" flex flex-col w-full pb-4">
-  //                 <p className="text-2xl font-bold">#</p>
-  //                 <p className="text-gray-600">com mais de 3 faturas</p>
-  //               </div>
-  //             </div>
-  //           </div>
-            
-  
-  //           <Link href='/novoCliente' className="btn btn-outline btn-success mt-3 mb-3">
-  //           <BiUserPlus/>
-  //           </Link>
-      
-            
-  //           <h1 className="text-center bold-400 text-xl mt-4 mb-3">Clientes</h1>
-  
 
     );
   }
