3c73143cc2321c5c56f8c9b235d3b8dc581f1e86
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-10 17:33:33 +0200

    alterrr

--- lib/cookiesConf.ts
+++ lib/cookiesConf.ts
@@ -4,7 +4,8 @@ import { cookies } from "next/headers";
 export async function getSession(){
     const session = await cookies().get('token')?.value;
     if(!session) return null 
-    return await (session) 
+    const token = await `Bearer ${session}`
+    return await (token) 
 }
 
 export async function getId(){
@@ -21,6 +22,7 @@ export async function getemail(){
 export async function getnome(){
     const idComp = await cookies().get('nome')?.value;
     if(!idComp) return null 
+    
     return await (idComp)
 }
 export async function getapelido(){
