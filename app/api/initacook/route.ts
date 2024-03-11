e5ff0b5bb3df6df673ebc8b95ea1265e0c2e20b5
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-11 16:09:37 +0200

    fghfff

--- app/api/initacook/route.ts
+++ app/api/initacook/route.ts
@@ -1,27 +1,27 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { cookies } from 'next/headers'
+// import { NextRequest, NextResponse } from 'next/server'
+// import { cookies } from 'next/headers'
 
  
 
-export async function POST(request: NextRequest) { 
-  const { searchParams } = new URL(request.url)
-  const idc = searchParams.get('_id')
+// export async function POST(request: NextRequest) { 
+//   const { searchParams } = new URL(request.url)
+//   const idc = searchParams.get('_id')
 
 
   // const resb = await request.json()
-  const res = await fetch(`https://agua-p.vercel.app/adm/fontcook/${idc}`,{
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
+  // const res = await fetch(`https://agua-p.vercel.app/adm/fontcook/${idc}`,{
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
       // 'API-Key': process.env.DATA_API_KEY,
-    },
+    // },
   //  body: JSON.stringify( resb )
-  })
+  // })
 
 
-      const data = await res.json();
-      const id   = await data.idComp
-      cookies().set('idCompan',id )
+  //     const data = await res.json();
+  //     const id   = await data.idComp
+  //     cookies().set('idCompan',id )
  
   // const data = await res.json()
   // const id   = await data.id
@@ -36,35 +36,29 @@ export async function POST(request: NextRequest) {
   // cookies().set('email' ,email)
   
 
-  return NextResponse.json(id)
-}
-
-
-//   import { NextRequest, NextResponse } from 'next/server'
-// import { getSession } from '@/lib/cookiesConf';
-// import { cookies } from 'next/headers'
-
-// export async function POST(request:NextRequest,{params}:any) {
-//   const ids = await params.ids 
-//   const idc = await ids[0]
-//   const idm = await ids[1]
-//   const body = await request.json();
-//   const token = await getSession()
-//   try {
-//     const response = await fetch(`https://agua-p.vercel.app/adm/fontcook/${idc}`,{
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         // 'Authorization' : `${token}`,
-//       },
-//       // body: JSON.stringify(body),
-//     });
-//     const data = await response.json();
-//     const id   = await data.idComp
-//     cookies().set('idCompan' , id )
-   
-//     return NextResponse.json(id); 
-//   } catch (error) {
-//     return NextResponse.json({ error: 'An error occurred' }, { status: 500 });
-//   }
+//   return NextResponse.json(id)
 // }
+
+
+import { NextRequest,NextResponse } from 'next/server';
+import { getSession } from '@/lib/cookiesConf';
+import { cookies } from 'next/headers'
+
+export async function GET(
+    request: Request,
+    { params }: { params: { id: string } }
+  ) {
+    const id = params.id 
+    const token = await getSession()
+
+    const res = await fetch(`https://agua-p.vercel.app/adm/fontcook/${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization' : `${token}`,
+      },
+    })
+    const clientes = await res.json()
+    const idcom   = await clientes.idComp
+    cookies().set('idComp' ,idcom)
+    return NextResponse.json( idcom )
+  }
