3c491e1bc8267f174dbc6a77e4994399a7cb25d5
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-12 13:31:09 +0200

    cade o id?

--- app/api/mes/[id]/route.ts
+++ app/api/mes/[id]/route.ts
@@ -1,15 +1,16 @@
 import { NextResponse,NextRequest } from "next/server"
-import { getCompan } from "@/lib/cookiesConf"
-import { getSession } from '@/lib/cookiesConf';
+import { getCompan } from '@/lib/cookiesConf'
+
 
 export async function GET(
     request: Request,
     { params }: { params: { id: string } }
   ) {
-    const idCom = await getCompan()
     
+    
+    const compId = await getCompan()
     const id = params.id 
-    const res = await fetch(`https://agua-p.vercel.app/adm/mesesGetbtcomp/${idCom}`,{cache:'no-store'})
+    const res = await fetch(`https://agua-p.vercel.app/adm/mesesGetbtcomp/${compId}`,{cache:'no-store'})
 
     const meses = await res.json()
     return NextResponse.json( meses )
