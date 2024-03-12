729b48e3724be7a5f9076bed762c0b64dd09e585
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-12 13:45:20 +0200

    dhfgh

--- app/api/mes/[id]/route.ts
+++ app/api/mes/[id]/route.ts
@@ -1,16 +1,21 @@
-import { NextResponse,NextRequest } from "next/server"
-import { getCompan } from '@/lib/cookiesConf'
+import { NextResponse,NextRequest } from "next/server";
+import { getCompan } from '@/lib/cookiesConf';
 
 
-export async function GET(
-    request: Request,
-    { params }: { params: { id: string } }
+export async function GET( request: Request, { params }: { params: { id: string } }
   ) {
     
     
-    const compId = await getCompan()
+    const idComp = await getCompan()
     const id = params.id 
-    const res = await fetch(`https://agua-p.vercel.app/adm/mesesGetbtcomp/${compId}`,{cache:'no-store'})
+    const res = await fetch(`https://agua-p.vercel.app/adm/mesesGetbtcomp/${idComp}`,{
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        // 'API-Key': process.env.DATA_API_KEY,
+      },
+    //  body: JSON.stringify( resb )
+    })
 
     const meses = await res.json()
     return NextResponse.json( meses )
