c12bf22dcf892e774cc888f806b910af38a39a30
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-12 14:19:20 +0200

    hfgdh

--- app/api/mes/route.ts
+++ app/api/mes/route.ts
@@ -3,10 +3,10 @@ import { getCompan } from '@/lib/cookiesConf'
 
 export async function GET(request: Request) {
     const compId = await getCompan()
-    const res = await fetch(`https://agua-p.vercel.app/adm/mesesGetbtcomp/${compId}`, {
+    const res = await fetch(`https://agua-p.vercel.app/adm/mesesGetbtcomp/65d4f7c0c2661d0c71a1e204`, {
       headers: {
         'Content-Type': 'application/json',
-        'API-Key': process.env.DATA_API_KEY!,
+        // 'API-Key': process.env.DATA_API_KEY!,
       },
     })
     const product = await res.json()
