a873d2486a066f84ad06a12b437e8b16b6fbfa53
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-12 12:57:48 +0200

    knlbkl.

--- app/api/mes/[id]/route.ts
+++ app/api/mes/[id]/route.ts
@@ -6,7 +6,7 @@ export async function GET(
     request: Request,
     { params }: { params: { id: string } }
   ) {
-    const idCom = `65a956bb8f3b0f53bed8bf8d`
+    const idCom = getCompan()
     const token = await getSession()
     const id = params.id 
     const res = await fetch(`https://agua-p.vercel.app/adm/mesesGetbtcomp/${idCom}`,{cache:'no-store'})
