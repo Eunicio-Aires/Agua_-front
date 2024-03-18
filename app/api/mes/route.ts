9b1981bdc6fde7a86d6203a266b1253c6bd7220b
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-18 07:42:00 +0200

    done cooks

--- app/api/mes/route.ts
+++ app/api/mes/route.ts
@@ -2,7 +2,7 @@ import { NextResponse,NextRequest } from "next/server";
 import { getCompan } from '@/lib/cookiesConf';
 export async function GET(request: NextRequest) {
     const idComp = await getCompan()
-    const res = await fetch(`https://agua-p.vercel.app/adm/mesesGetbtcomp/65a956bb8f3b0f53bed8bf8d`,)
+    const res = await fetch(`https://agua-p.vercel.app/adm/mesesGetbtcomp/${idComp}`,)
     const data = await res.json()
    
     return NextResponse.json( data)
