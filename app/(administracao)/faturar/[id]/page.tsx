8e91e8cc51c651daec407291cdc4b66817a37a21
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-18 14:57:12 +0200

    kerndfkl cd

--- app/(administracao)/faturar/[id]/page.tsx
+++ app/(administracao)/faturar/[id]/page.tsx
@@ -20,13 +20,13 @@ interface Mes {
 export default async function Page({ params }: { params: { id: string } }) {
     const baseUrl = process.env.NEXT_LOCAL_BASE_URL;
     const posts = await fetch(`${baseUrl}/api/mesUnic/${params.id}`,{cache:'no-store'}).then((res) => res.json());
-    // const mes = await posts.mes._id;
+    const mes = await posts.mes._id;
 
     return (
        <div  className="flex" >
             {/* <h1 className="text-center text-xl font-bold tracking-normal sm:text-4xl lg:text-4xl mt-1">{posts.mes.mes}</h1>  */}
             <p>klnklnklj;jujhuiohuh</p>
-        {/* <Procurarc _id={mes}/> */}
+        <Procurarc _id={mes}/>
         </div>
             
     ) 
