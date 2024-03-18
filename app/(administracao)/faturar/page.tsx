7fe521174e2cd9d8385bccf905982f54d53c0b71
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-18 06:05:15 +0200

    tarde de doming

--- app/(administracao)/faturar/page.tsx
+++ app/(administracao)/faturar/page.tsx
@@ -13,7 +13,7 @@ export default async function Faturar(){
   return(
     <div className="">
     
-   <p>{meses}</p>
+   {/* <p>{meses}</p> */}
 
      <section className="py-1 bg-blueGray-50">
 <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
