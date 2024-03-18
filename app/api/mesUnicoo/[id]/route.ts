f0ff13a347e97dcaf62fadc2ca363c07fc7cfece
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-18 14:04:15 +0200

    Get alterado

--- app/api/mesUnicoo/[id]/route.ts
+++ app/api/mesUnicoo/[id]/route.ts
@@ -1,12 +1,24 @@
 import { NextResponse, NextRequest } from "next/server"
 import { getSession } from "@/lib/cookiesConf"
-export async function GET(
-    request: NextRequest,
-    { params }: { params: { id: string } }
-  ) {
-    // const token = getSession()
-    const id = params.id 
-    const res = await fetch(`https://agua-p.vercel.app/adm/mesunic/${id}`)
-    const clientes = await res.json()
-    return NextResponse.json( clientes )
+// export async function GET(
+//     request: NextRequest,
+//     { params }: { params: { id: string } }
+//   ) {
+//     // const token = getSession()
+//     const id = params.id 
+//     const res = await fetch(`https://agua-p.vercel.app/adm/mesunic/${id}`,{cache:'no-store'})
+//     const clientes = await res.json()
+//     return NextResponse.json( clientes )
+//   }
+
+
+  export async function GET( request: Request, { params }: { params: { id: string }}) {
+    
+    
+    
+    const id = params.id;
+    const res = await fetch(`https://agua-p.vercel.app/adm/mesunic/${id}`,)
+
+    const meses = await res.json()
+    return NextResponse.json(meses)
   }
