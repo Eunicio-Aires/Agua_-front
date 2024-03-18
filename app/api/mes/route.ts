1fec02afe43fb344c94e47f2fed7c6dd5dec0fa5
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-18 05:55:22 +0200

    validacaooo

---
+++ app/api/mes/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse,NextRequest } from "next/server";
+import { getCompan } from '@/lib/cookiesConf';
+export async function GET(request: Request) {
+    const idComp = await getCompan()
+    const res = await fetch(`https://agua-p.vercel.app/adm/mesesGetbtcomp/${idComp}`,)
+    const data = await res.json()
+   
+    return NextResponse.json({ data} )
+  }
