eb6a719e446d2ed06a151d523bd99a71130e3b7d
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-18 06:07:46 +0200

    objeretirado

--- app/api/mes/route.ts
+++ app/api/mes/route.ts
@@ -5,5 +5,5 @@ export async function GET(request: Request) {
     const res = await fetch(`https://agua-p.vercel.app/adm/mesesGetbtcomp/${idComp}`,)
     const data = await res.json()
    
-    return NextResponse.json({ data} )
+    return NextResponse.json( data)
   }
