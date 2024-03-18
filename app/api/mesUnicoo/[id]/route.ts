671590a5e52ea1f9a8cbf6b846673a96cc151ba7
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-18 12:16:23 +0200

    done mes unico

--- app/api/mesUnicoo/[id]/route.ts
+++ app/api/mesUnicoo/[id]/route.ts
@@ -4,14 +4,9 @@ export async function GET(
     request: NextRequest,
     { params }: { params: { id: string } }
   ) {
-    const token = getSession()
+    // const token = getSession()
     const id = params.id 
-    // const toke = await `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YWE3NzRjMGUyODA5OTAwMmYxNThlMyIsImlhdCI6MTcwOTgyMTA3MSwiZXhwIjoxNzA5OTA3NDcxfQ.oeKHAXmxlcfCFwSyBc7A0HxlSrH2vrjfgaZj-mKk3-8`
-    const res = await fetch(`https://agua-p.vercel.app/adm/mesunic/65a956bb8f3b0f53bed8bf8d`,{
-        headers: {
-          'Authorization' :`${token}`,
-        }
-      })
+    const res = await fetch(`https://agua-p.vercel.app/adm/mesunic/${id}`)
     const clientes = await res.json()
     return NextResponse.json( clientes )
   }
