e3af7d87d7f7555de9d214b155ff82c2d15e729c
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-18 08:12:07 +0200

    tokennnnsdf

--- app/api/mes/[id]/route.ts
+++ app/api/mes/[id]/route.ts
@@ -10,6 +10,6 @@ export async function GET( request: Request, { params }: { params: { id: string 
     const res = await fetch(`https://agua-p.vercel.app/adm/mesesGetbtcomp/${idComp}`,)
 
     const meses = await res.json()
-    return NextResponse.json("meses")
+    return NextResponse.json(meses)
   }
 
