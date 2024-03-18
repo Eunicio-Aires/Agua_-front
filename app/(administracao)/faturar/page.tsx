104ce2a05d24743c2fc4f545bf88c173cbcfb729
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-18 06:43:04 +0200

    nome note finddd

--- app/(administracao)/faturar/page.tsx
+++ app/(administracao)/faturar/page.tsx
@@ -20,7 +20,7 @@ export default async function Faturar(){
   return(
     <div className="">
     
-   <p>{nome}</p>
+   {/* <p>{nome}</p> */}
 
      <section className="py-1 bg-blueGray-50">
 <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
