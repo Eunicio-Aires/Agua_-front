870299c98eb4cf5192a3ba9ecc194851bf6443a9
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-21 01:10:14 +0200

    fedsdf

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -150,8 +150,8 @@ export default function SideBar(id:any){
               </button>
             </div>
             <div id="menu" className="flex flex-col space-y-2">
-              <Link
-                href={`inicial/${id}`}
+              <a
+                href= {`/inicial/${id}`}
                 className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:text-base rounded-md transition duration-150 ease-in-out"
               >
                 <svg
@@ -165,9 +165,9 @@ export default function SideBar(id:any){
                   ></path>
                 </svg>
                 <span className="">Home</span>
-              </Link>
+              </a>
               <a
-                href=""
+                href={`/faturar`}
                 className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
               >
                 <svg
@@ -180,10 +180,10 @@ export default function SideBar(id:any){
                     d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z"
                   ></path>
                 </svg>
-                <span className="">Products</span>
+                <span className="">Faturar</span>
               </a>
               <a
-                href=""
+                href={`/pagaments`}
                 className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
               >
                 <svg
@@ -199,7 +199,7 @@ export default function SideBar(id:any){
                     clip-rule="evenodd"
                   ></path>
                 </svg>
-                <span className="">Reports</span>
+                <span className="">Pagamentos</span>
               </a>
               <a
                 href=""
