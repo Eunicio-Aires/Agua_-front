5351c134b18aa4263a3ea8c7781d43cfd88f86ae
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-22 00:23:45 +0200

    dois dos quais tem

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -103,7 +103,7 @@ export default async function SideBar(){
         </button> */}
         <div
           id="sidebar"
-          className="bg-white h-screen md:block shadow-xl px-3 w-30 md:w-60 lg:w-60 overflow-x-hidden transition-transform duration-300 ease-in-out"
+          className="bg-white h-screen  shadow-xl px-3 w-30 md:w-60 lg:w-60 overflow-x-hidden transition-transform duration-300 ease-in-out"
           x-show="sidenav"
           
         >
