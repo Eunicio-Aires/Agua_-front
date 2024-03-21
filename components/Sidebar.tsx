aac743b3c5121907b676a546b1cf13352ef788e2
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-22 01:14:53 +0200

    hkhlkhn

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -80,7 +80,7 @@ export default async function SideBar(){
     return <div className=' fixed flex flex-col gap-3 w-[250px] min-w-[250px]  '>
 
         <div
-        // id="view"
+        id="view"
         className="h-[750px] w-screen flex flex-row"
         x-data="{ sidenav: true }"
       >
