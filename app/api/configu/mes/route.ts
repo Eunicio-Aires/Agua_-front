9b36c70ab161ff9155af3da7dd20340b1792c3b1
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-25 16:42:52 +0200

    rg;hk;ndfv

--- app/api/configu/mes/route.ts
+++ app/api/configu/mes/route.ts
@@ -1,11 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { cookies } from "next/headers";
+import { getCompan } from '@/lib/cookiesConf';
 
 
 export async function POST(request: NextRequest) {
-    const idComp = await cookies().get('idComp')
+   const idcp = await getCompan()
     const body = await request.json();
-    const res = await fetch(`https://agua-p.vercel.app/adm/criarMes/${idComp}`, {
+    const res = await fetch(`https://agua-p.vercel.app/adm/criarMes/${idcp}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -14,6 +15,7 @@ export async function POST(request: NextRequest) {
       body: JSON.stringify(body),
     })
    
+    
     const data = await res.json()
    
     return NextResponse.json(data)
