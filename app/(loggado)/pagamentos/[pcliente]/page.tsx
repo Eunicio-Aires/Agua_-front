8d53fa8bdb45958b77d8834aa28d9f81dc202620
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-28 16:22:17 +0200

    kdsfhjkdfskjbkdfjs

--- app/(loggado)/pagamentos/[pcliente]/page.tsx
+++ app/(loggado)/pagamentos/[pcliente]/page.tsx
@@ -41,7 +41,7 @@ export default async function Page({ params }: { params: {pcliente: Cliente}}){
       <div className="pt-1 min-h-screen bg-gradient-to-tr from-gray-50  via-slate-100 to-cyan-200">
           <div className=" bg-opacity-70 bg-white bg-blur-md shadow-lg rounded-lg  min-w-98 max-w-98  mb-8">
           <Link  href={`/`}> <CiMenuBurger className=' '/></Link> 
-                <h2 className ="text-center bg-gradient-to-r from-slate-100 to-indigo-700 bg-clip-text text-transparent text-xl font-bold ">{cliente.client.nome}</h2>
+                <h2 className ="text-center bg-gradient-to-r from-slate-100 to-indigo-700 bg-clip-text text-transparent text-xl font-bold ">{cliente.client.nome} {cliente.client.apelido}</h2>
                 <p className ="text-center mb-1" >Aguas Mulungo</p>
 
                 <div className='flex justify-between px-4 pt-4 '>
