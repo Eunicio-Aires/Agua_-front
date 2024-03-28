0b10d0b2074e431cbd4541e90885a982c56cfd97
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-03-28 12:12:15 +0200

    dsfdvvc

--- app/(loggado)/pagamentos/[pcliente]/page.tsx
+++ app/(loggado)/pagamentos/[pcliente]/page.tsx
@@ -39,10 +39,8 @@ export default async function Page({ params }: { params: {pcliente: Cliente}}){
     return(
 
       <div className="pt-1 min-h-screen bg-gradient-to-tr from-gray-50  via-slate-100 to-cyan-200">
-       <Link href={`/`}><FcAbout /></Link> 
-
           <div className="pt-2 bg-opacity-70 bg-white bg-blur-md shadow-lg rounded-lg  w-full ml-2 mr-2 mb-8">
-
+          <Link className='w-10' href={`/`}><FcAbout /></Link> 
                 <h2 className ="text-center bg-gradient-to-r from-slate-100 to-indigo-700 bg-clip-text text-transparent text-xl font-bold ">{cliente.client.nome}</h2>
                 <p className ="text-center mb-1" >Aguas Mulungo</p>
 
@@ -58,7 +56,7 @@ export default async function Page({ params }: { params: {pcliente: Cliente}}){
 
 
 
-          <div className="fixed mt-36">
+          <div className="fixed mt-16">
           <Image className ="opacity-40 "
           alt="hands"
          
@@ -72,7 +70,7 @@ export default async function Page({ params }: { params: {pcliente: Cliente}}){
           />
           </div>
 
-          <section className="flex flex-wrap justify-center mt-32 absolute w-full">
+          <section className="flex flex-wrap justify-center mt-12 absolute w-full">
           
               <div  className="w-full md:w-1/3 p-4">
                 {fatur.map((zon:any) => ( 
