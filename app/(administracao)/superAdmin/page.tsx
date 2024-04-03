68b61577621e8638e415df2bc1f2d13aa7919d49
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-04-03 20:01:12 +0200

    dsfsdfdff

--- app/(administracao)/superAdmin/page.tsx
+++ app/(administracao)/superAdmin/page.tsx
@@ -67,7 +67,7 @@ export default async function SuperAdm(){
 
         <tbody>
         {admins.map((companes:any)=>(
-          <tr>
+          <tr key={companes._id}>
             <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
             {companes.nome} {companes.apelido}
             </th>
