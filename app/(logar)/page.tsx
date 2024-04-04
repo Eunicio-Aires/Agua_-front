439a0f23148bb087805cb4aeb5c9db24781c4362
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-04-04 06:25:49 +0200

    jkgjk

--- app/(logar)/page.tsx
+++ app/(logar)/page.tsx
@@ -1,4 +1,4 @@
-import hands from '../../../../public/hands4903050-1280e16238370891651090x613removebgpreview-1@2x copy.png';
+// import hands from '../../../../public/hands4903050-1280e16238370891651090x613removebgpreview-1@2x copy.png';
 import Image from 'next/image'
 
 import { Log } from '@/components/Log'
@@ -8,7 +8,7 @@ export default function Login() {
   return (
     <div >
 
-<div className="fixed mt-6 min-w-full">
+{/* <div className="fixed mt-6 min-w-full">
           <Image className ="opacity-40 min-w-full"
           alt="hands"
          
@@ -20,7 +20,7 @@ export default function Login() {
             height: 'auto',
           }}
           />
-          </div>
+          </div> */}
 
 
   <Log/>
