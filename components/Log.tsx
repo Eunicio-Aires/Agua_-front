e31e273d4a8e31215eefbf2f48775d5d8164c475
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-04-04 06:37:14 +0200

    jhfgkhjg

--- components/Log.tsx
+++ components/Log.tsx
@@ -61,7 +61,7 @@ export  function Log() {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Entrar!</h1>
-          <p className="py-6">Faca a consulta e pagamento das  <br/>  faturas de agua, em  qualquer canto do mundo</p>
+          <p className="py-6">Faça a consulta e pagamento das  <br/>  faturas de Água, em  qualquer canto do mundo</p>
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
