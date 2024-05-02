ad0a52534c871d5927ee8ff3071f66b0426d2213
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-05-02 14:27:25 +0200

    gyyjjy

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -64,7 +64,7 @@ export default async function SideBar(){
             
             <div id="menu" className="flex flex-col space-y-2">
               <Link
-                href= {`inicial/${empre}`}
+                href= {`/inicial/${empre}`}
                 className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:text-base rounded-md transition duration-150 ease-in-out"
               >
                 <svg
